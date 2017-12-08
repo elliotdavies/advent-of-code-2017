2c92261f3bf567a452723ec44fe808a727b83bd9
Elliot Davies <elliot.davies@the-times.co.uk>
2017-12-08 00:34:03 +0000

    Day 7 part 2 nearly there...

--- day7/src/Lib.hs
+++ day7/src/Lib.hs
@@ -1,17 +1,61 @@
 module Lib where
 
-import Data.List (union)
+import Data.List (sort, sortOn)
+import Debug.Trace (trace)
 
 import Input (input)
 
-findRoot :: [(String, Int, [String])] -> String
+type Node = (String, Int, [String])
+
+name :: Node -> String
+name (n,_,_) = n
+
+weight :: Node -> Int
+weight (_,w,_) = w
+
+branches :: Node -> [String]
+branches (_,_,ns) = ns
+
+-- Part 1
+findRoot :: [Node] -> Node
 findRoot nodes =
   let
-    names = map (\(n,_,_) -> n) nodes
-    pointedTo = nodes >>= (\(_,_,xs) -> xs)
+    pointedTo = nodes >>= branches
 
   in
-    head $ filter (not . (\n -> n `elem` pointedTo)) names
+    head $ filter (not . (flip elem $ pointedTo) . name) nodes
 
 solution1 :: String
-solution1 = findRoot input
+solution1 = name $ findRoot input
+
+-- Part 2
+findImbalance :: Node -> [Node] -> Int
+findImbalance node nodes =
+  let
+    weights =
+      zip (map weight $ findBranches (branches node)) (branchWeights (branches node))
+
+  in
+    correction $ sortOn snd weights
+
+  where
+    findBranches :: [String] -> [Node]
+    findBranches names =
+      filter (\n -> name n `elem` names) nodes
+
+    branchWeights :: [String] -> [Int]
+    branchWeights names =
+      map (\n -> weight n + sum (branchWeights $ branches n)) $ findBranches names
+
+    correction :: [(Int,Int)] -> Int
+    correction [] = 0
+    correction (_:[]) = 0
+    correction ((w,bw):(w',bw'):ws) =
+      if bw > bw'
+        then w - (bw - bw')
+        else if bw < bw'
+          then w + (bw' - bw)
+          else correction $ reverse ((w',bw'):ws)
+
+solution2 :: Int
+solution2 = findImbalance (findRoot input) input
--- day7/test/Spec.hs
+++ day7/test/Spec.hs
@@ -6,27 +6,38 @@ main = do
   runTestTT tests
   return ()
 
+input :: [Lib.Node]
+input =
+  [ ("pbga", 66, []),
+    ("xhth", 57, []),
+    ("ebii", 61, []),
+    ("havc", 66, []),
+    ("ktlj", 57, []),
+    ("fwft", 72, ["ktlj", "cntj", "xhth"]),
+    ("qoyq", 66, []),
+    ("padx", 45, ["pbga", "havc", "qoyq"]),
+    ("tknk", 41, ["ugml", "padx", "fwft"]),
+    ("jptl", 61, []),
+    ("ugml", 68, ["gyxo", "ebii", "jptl"]),
+    ("gyxo", 61, []),
+    ("cntj", 57, []) ]
+
 tests = TestLabel "tests" $ test [
 
     TestLabel "findRoot" $ test [
 
       "example" ~: do
-        let
-          input =
-            [ ("pbga", 66, []),
-              ("xhth", 57, []),
-              ("ebii", 61, []),
-              ("havc", 66, []),
-              ("ktlj", 57, []),
-              ("fwft", 72, ["ktlj", "cntj", "xhth"]),
-              ("qoyq", 66, []),
-              ("padx", 45, ["pbga", "havc", "qoyq"]),
-              ("tknk", 41, ["ugml", "padx", "fwft"]),
-              ("jptl", 61, []),
-              ("ugml", 68, ["gyxo", "ebii", "jptl"]),
-              ("gyxo", 61, []),
-              ("cntj", 57, []) ]
         let expected = "tknk"
-        assertEqual "" expected (findRoot input)
+        assertEqual "" expected (name $ findRoot input)
+
+      ],
+
+    TestLabel "findImbalance" $ test [
+
+      "example" ~: do
+        let expected = 60
+        let root = findRoot input
+        assertEqual "" expected (findImbalance root input)
+
       ]
     ]
